10a29ffd6862dae2ceec61cd8eddfd158f8417aa
Jeremy McClelland <jeremy@moraycreative.com>
2018-10-25 08:47:37 -0400

    added google tag manager

--- wp-content/themes/muze/header.php
+++ wp-content/themes/muze/header.php
@@ -11,6 +11,24 @@
 <!--[if IE 9]><html class="no-js lt-ie10" lang="en"> <![endif]-->
 <html lang="en" xml:lang="en">
 	<head>
+		
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-128081557-1"></script>
+
+<script>
+
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag(){dataLayer.push(arguments);}
+
+  gtag('js', new Date());
+
+ 
+
+  gtag('config', 'UA-128081557-1');
+
+</script>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<title><?php bloginfo('name'); ?> | <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
